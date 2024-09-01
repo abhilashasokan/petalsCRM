15f772c50ed516c581a838e04e8de14882cc1bad
AbhilashAsokan <abhilash.asokan@logicline.de>
2024-09-01 12:12:20 +0530

    :sparkles: initial commit

---
+++ src/main/java/com/petals/fashions/petalscrm/PetalscrmApplication.java
@@ -0,0 +1,13 @@
+package com.petals.fashions.petalscrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetalscrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetalscrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petals/fashions/petalscrm/PetalscrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petals.fashions.petalscrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetalscrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
